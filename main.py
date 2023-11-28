c421efa2f61ae42f2a37e3bafea65153d91b62dc
arif <arifur.tuc@gmail.com>
2023-11-28 19:12:19 +0100

    Add table widget for student data display

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from PyQt6.QtWidgets import QBoxLayout, QApplication, QWidget, QLabel, \
-    QGridLayout, QLineEdit, QPushButton, QMainWindow
+    QGridLayout, QLineEdit, QPushButton, QMainWindow, QTableWidget
 from PyQt6.QtGui import QAction
 import sys
 
@@ -9,17 +9,30 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle("Student Management System")
 
+        # Creating File and Help menus in the menu bar
         file_menu_item = self.menuBar().addMenu("&File")
         help_menu_item = self.menuBar().addMenu("&Help")
 
+        # Adding actions to File menu
         add_student_action = QAction("Add Student", self)
         file_menu_item.addAction(add_student_action)
 
+        # Adding actions to Help menu
         about_action = QAction("About", self)
         help_menu_item.addAction(about_action)
         about_action.setMenuRole(QAction.MenuRole.NoRole)
 
+        # Creating a table widget for student data display
+        self.table = QTableWidget()
+        self.table.setColumnCount(4)
+        self.table.setHorizontalHeaderLabels(("Id", "Name", "Course", "Mobile"))
+        self.setCentralWidget(self.table)
 
+    def load_data(self):
+        pass
+
+
+# Creating the application and main window
 app = QApplication(sys.argv)
 window = MainWindow()
 window.show()
